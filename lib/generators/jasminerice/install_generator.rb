8f9c340380fde26593d96234f8ff8d2898c863c8
Nick DeSteffen <nick.desteffen@gmail.com>
2012-12-16 15:05:45 -0600

    Removing Haml dependency

--- lib/generators/jasminerice/install_generator.rb
+++ lib/generators/jasminerice/install_generator.rb
@@ -9,7 +9,7 @@ if ::Rails.version >= "3.1"
         copy_file "spec.js.coffee", "spec/javascripts/spec.js.coffee"
         copy_file 'foo_spec.js.coffee', 'spec/javascripts/foo_spec.js.coffee'
         copy_file 'spec.css', 'spec/javascripts/spec.css'
-        copy_file 'baz.html.haml', 'spec/javascripts/fixtures/baz.html.haml'
+        copy_file 'baz.html.erb', 'spec/javascripts/fixtures/baz.html.erb'
       end
     end
   end
